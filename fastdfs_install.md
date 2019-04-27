f3692ff6c37702c186ad65c4a2abe5bd4fbc416b
Andrew <wj.andrew@yahoo.com>
2019-04-27 12:56:50 +0800

    update

--- fastdfs_install.md
+++
@@ -1,2 +0,0 @@
-# FastDFS安装.md
-
---
+++ reward.js
@@ -0,0 +1,6 @@
+window.document.onload=funcction(e) {
+	var html = "<div class='reward'>" +
+					"<div style='text-align: center; display: inline-block; '><img width='250' height='250' src='https://snowthinker.github.io/res/wechat_pay.jpeg'/> &nbsp; &nbsp;&nbsp; <img width='250' height='250' src='https://snowthinker.github.io/res/alipay.jpeg'/></div>" +
+			   "<div>";
+	document.body.appendChild(html);
+}
